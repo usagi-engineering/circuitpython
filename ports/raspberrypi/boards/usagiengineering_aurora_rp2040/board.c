7e0413eea6fa112d8d6744459efefe9997732998
Zachary Hart <zach@zthart.me>
2023-03-27 16:55:20 -0700

    add Aurora to rp2040 ports

---
+++ ports/raspberrypi/boards/usagiengineering_aurora_rp2040/board.c
@@ -0,0 +1,1 @@
+#include "supervisor/board.h"
---
+++ ports/raspberrypi/boards/usagiengineering_aurora_rp2040/mpconfigboard.mk
@@ -0,0 +1,12 @@
+USB_VID = 0x1209
+USB_PID = 0x54A0
+USB_PRODUCT = "Aurora"
+USB_MANUFACTURER = "Usagi Engineering"
+
+CHIP_VARIANT = "RP2040"
+CHIP_FAMILY = rp2
+
+EXTERNAL_FLASH_DEVICES = "W25Q64JVxQ"
+
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_HID
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Register
